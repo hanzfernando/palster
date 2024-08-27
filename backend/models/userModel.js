d2b9795fbe21cc5dd384b48fe596d184277ebe50
hanzfernando <fernandohanz23@gmail.com>
2024-08-27 16:53:49 +0800

    initialized react in frontend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,9 @@ const userSchema = new mongoose.Schema(
         verificationToken: {
             type: String,
         },
+        avatar: {
+            type
+        }
     },
     {
         timestamps: true
