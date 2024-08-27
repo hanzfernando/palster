38ffbbe27823204dc3d10c2ebd71702f95a28129
hanzfernando <fernandohanz23@gmail.com>
2024-08-27 17:03:17 +0800

    added a default avatar icon

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,7 +30,8 @@ const userSchema = new mongoose.Schema(
             type: String,
         },
         avatar: {
-            type
+            type: String,
+            default: 'https://drive.google.com/file/d/1RGRmjbYkF7zYIYRbQjg7FQbhFLDCsASZ/view?usp=sharing',
         }
     },
     {
